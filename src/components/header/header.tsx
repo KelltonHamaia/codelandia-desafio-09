d46d780bee471a76d31a05a55c02b34007ef94d2
kellton hamaia <hamaia.kell@gmail.com>
2024-04-10 17:59:42 -0300

    refactor: remove unused classes and add animation when hover a menu link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
             <Link href="/" className="flex items-center gap-2 text-sm font-bold md:text-3xl">
                 <img
                     src="/images/brands/brand-alt.svg"
-                    className="hidden size-12 md:block"
+                    className="size-12"
                     alt="logo"
                 />
                 &#123;Mkt.dev&#125;
--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -5,16 +5,16 @@ export const Menu = () => {
 
     return (
         <ul className="items-center hidden gap-5 lg:flex">
-            <li className="text-xl">
+            <li className="text-xl hover:text-primary hover:animate-pulse">
                 <Link href="#about-me">Sobre mim</Link>
             </li>
-            <li className="text-xl">
+            <li className="text-xl hover:text-primary hover:animate-pulse">
                 <Link href="#projects">Projetos</Link>
             </li>
-            <li className="text-xl">
+            <li className="text-xl hover:text-primary hover:animate-pulse">
                 <Link href="#jobs">Serviços</Link>
             </li>
-            <li className="text-xl">
+            <li className="text-xl hover:text-primary hover:animate-pulse">
                 <Link href="#skills">Minhas skills</Link>
             </li>
             <ThemeSwitch />
